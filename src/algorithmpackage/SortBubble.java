05cc204eb6d63422ebba330fa377f7c6edc316ab
lost273 <dnk273@gmail.com>
2017-12-27 15:41:30 +0300

    27-12-2017

--- src/algorithmpackage/SortBubble.java
+++ src/algorithmpackage/SortBubble.java
@@ -2,19 +2,12 @@
 package algorithmpackage;
 
 public class SortBubble {
-    public static void main(String[] args){
-        int nums[] = {99, -10, 100123, 18, -978,
-                     5623, 463, -9, 287, 49};
+    static void bsort(char items[]){
+        qs(items, 0, items.length-1);
+    }
         int a, b, t;
         int size;
-        //number of elements
-        size = 10;
-        //display the array
-        System.out.print("current array:");
-        for(int i = 0; i < size; i++){
-            System.out.print(" " + nums[i]);
-        }
-        System.out.println();
+        
         //algorithm
         for(a = 1; a < size; a++){
             for(b = size - 1; b >= a; b--){
@@ -25,6 +18,25 @@ public class SortBubble {
                 }
             }
         }
+        
+    
+}
+class Start{
+    public static void main(String[] args){
+        int nums[] = {99, -10, 100123, 18, -978,
+                     5623, 463, -9, 287, 49};
+        //number of elements
+        size = 10;
+        
+        //display the array
+        System.out.print("current array:");
+        for(int i = 0; i < size; i++){
+            System.out.print(" " + nums[i]);
+        }
+        System.out.println();
+        
+        SortBubble.bsort(nums);
+        
         //display the array
         System.out.print("sorted array:");
         for(int i = 0; i < size; i++){
