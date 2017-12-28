9b353c4fbb558cca3fe05d5115d450ae67536498
lost273 <dnk273@gmail.com>
2017-12-28 16:07:58 +0300

    28-12-2017

--- src/algorithmpackage/SortSelect.java
+++ src/algorithmpackage/SortSelect.java
@@ -2,39 +2,45 @@
 package algorithmpackage;
 
 public class SortSelect {
-static void selectSort(int a[]) {
-  long i, j, k; 
-  int x[];
-  // i - number of the current step
-  for( i=0; i < size; i++) {
-    k=i; x=a[i];
-    // the cycle for selecting the smallest element
-    for( j=i+1; j < size; j++)	
-      if (  a[j] < x ) {
-          // k - index of the smallest element
-        k=j; x=a[j];	        
-      }
-    // swap places least
-    a[k] = a[i]; a[i] = x;   	
-  }
-}
+    static void selectSort(int a[]) {
+        int i, j, k;
+        int x;
+        // i - number of the current step
+        for( i = 0; i < a.length; i++) {
+            k = i;
+            x = a[i];
+            // the cycle for selecting the smallest element
+            for( j = i+1; j < a.length; j++){
+                if (a[j] < x) {
+                    // k - index of the smallest element
+                    k = j;
+                    x = a[j];
+                }
+            }
+            // swap places least
+            a[k] = a[i]; 
+            a[i] = x;
+        }
+    }
 }
 class StartSortSelect{
     public static void main(String[] args){
         int nums[] = {99, -10, 100123, 18, -978,
                      5623, 463, -9, 287, 49};
-        int i;
-        
+        //display the array
         System.out.print("current array:");
-        for(i = 0; i < nums.length; i++){
-            System.out.print(nums[i]);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(" " + nums[i]);
         }
         System.out.println();
-        SortSelect.selectSort(nums);
         
+        SortBubble.bsort(nums);
+        
+        //display the array
         System.out.print("sorted array:");
-        for(i = 0; i < nums.length; i++){
-            System.out.print(nums[i]);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(" " + nums[i]);
         }
+        System.out.println();
     }
 }
