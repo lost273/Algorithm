c5a6e0d97ca8190e3866a95cc7aaa2b87bf4e3dc
lost273 <dnk273@gmail.com>
2018-01-01 19:25:15 +0300

    01-01-2018

--- src/algorithmpackage/SortShell.java
+++ src/algorithmpackage/SortShell.java
@@ -11,36 +11,31 @@ public class SortShell extends Algorithm {
         s = -1;
         
         do {
-            if (++s % 2) {
-      inc[s] = 8*p1 - 6*p2 + 1;
-    } else {
-      inc[s] = 9*p1 - 9*p3 + 1;
-      p2 *= 2;
-      p3 *= 2;
+            if ((++s % 2) == 0) {
+                inc[s] = 8*p1 - 6*p2 + 1;
+            } else {
+                inc[s] = 9*p1 - 9*p3 + 1;
+                p2 *= 2;
+                p3 *= 2;
+            }
+            p1 *= 2;
+        } while(3*inc[s] < size);
+        return s > 0 ? --s : 0;
     }
-	p1 *= 2;
-  } while(3*inc[s] < size);  
-
-  return s > 0 ? --s : 0;
-}
-
-template<class T>
-void shellSort(T a[], long size) {
-  long inc, i, j, seq[40];
-  int s;
-
- 
-  s = increment(seq, size);
-  while (s >= 0) {
-	
-	inc = seq[s--];
-
-    for (i = inc; i < size; i++) {
-      T temp = a[i];
-      for (j = i-inc; (j >= 0) && (a[j] > temp); j -= inc)
-        a[j+inc] = a[j];
-      a[j+inc] = temp;
+    
+    public void Sort(int a[]) {
+        int inc, i, j, s;
+        int seq[] = new int[40];
+        s = increment(seq, a.length);
+        while (s >= 0) {
+            inc = seq[s--];
+            for (i = inc; i < a.length; i++) {
+                int temp = a[i];
+                for (j = i - inc; (j >= 0) && (a[j] > temp); j -= inc){
+                    a[j+inc] = a[j];
+                }
+                a[j+inc] = temp;
+            }
+        }
     }
-  }
-}
 }
---
+++ test/testpackage/SortShellTest.java
@@ -0,0 +1,11 @@
+
+package testpackage;
+
+import modelpackage.*;
+import algorithmpackage.*;
+
+public class SortShellTest {
+    public static void main(String[] args){
+        TestModel.StartIntTest(new SortShell());
+    }
+}
