becb831ffd234dc99af9c3a8a325ab3032f7ade3
lost273 <dnk273@gmail.com>
2018-01-02 22:00:41 +0300

    02-01-2018

---
+++ test/testpackage/SortHeapTest.java
@@ -0,0 +1,11 @@
+
+package testpackage;
+
+import modelpackage.*;
+import algorithmpackage.*;
+
+public class SortHeapTest {
+    public static void main(String[] args){
+        TestModel.StartIntTest(new SortHeap());
+    }
+}
