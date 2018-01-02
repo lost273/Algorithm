f37d48c034ba2402392d7e5f6c5c9d49a9773344
lost273 <dnk273@gmail.com>
2018-01-02 21:57:36 +0300

    02-01-2018

--- src/algorithmpackage/SortHeap.java
+++ src/algorithmpackage/SortHeap.java
@@ -5,4 +5,37 @@ public class SortHeap  extends Algorithm {
     public SortHeap(){
         super("SortHeap");
     }
+    void downHeap(int a[], int k, int n) {
+        //  procedure for sifting the next element
+        //  before: a[k+1]...a[n]  - pyramid
+        //  after:  a[k]...a[n]  - pyramid
+        int new_elem, child;
+        new_elem = a[k];
+        while(k <= n/2) {
+            child = 2*k;
+            if( child < n && a[child] < a[child+1] ){
+                child++;
+            }
+            if( new_elem >= a[child] ) break;
+            a[k] = a[child];
+            k = child;
+        }
+        a[k] = new_elem;
+    }
+    @Override
+    public void Sort(int a[]) {
+        int i, temp;
+        //build a pyramid
+        for(i = a.length/2 - 1; i >= 0; i--) {
+            downHeap(a, i, a.length-1);
+        }
+        for(i = a.length - 1; i > 0; i--) {
+            // change first with last
+            temp = a[i]; 
+            a[i] = a[0]; 
+            a[0] = temp;
+            // restore the pyramid a[0]...a[i-1] 
+            downHeap(a, 0, i-1); 
+        }
+    }
 }
