21947f0da9991082c5239e6ddd64f936d2d81c32
lost273 <dnk273@gmail.com>
2018-01-04 19:07:57 +0300

    04-01-2018

--- src/displaypackage/Display.java
+++ src/displaypackage/Display.java
@@ -2,14 +2,7 @@ package displaypackage;
 
 public class Display {
     //display the array
-    public static void IntDisplay(String msg, int nums[]){
-        System.out.print(msg);
-        for(int i = 0; i < nums.length; i++){
-            System.out.print(" " + nums[i]);
-        }
-        System.out.println();
-    }
-    public static void CharDisplay(String msg, char nums[]){
+    public static <T> void ConDisplay(String msg, T[] nums){
         System.out.print(msg);
         for(int i = 0; i < nums.length; i++){
             System.out.print(" " + nums[i]);
--- test/modelpackage/TestModel.java
+++ test/modelpackage/TestModel.java
@@ -5,7 +5,7 @@ import algorithmpackage.*;
 import displaypackage.*;
 
 public class TestModel {
-    private static String IntCompare(int unsorted[], int sorted[]){
+    private static <T> String IntCompare(T[] unsorted, T[] sorted){
         for(int i = 0; i < unsorted.length; i++){
             if(unsorted[i] != sorted[i]){
                 return "FAIL";
@@ -14,20 +14,21 @@ public class TestModel {
         return "SUCCESS";
     }
     public static void StartIntTest(Algorithm algorithm){
-        int nums[] = {99, -10, 100123, 18, -978,
+        Integer[] nums = {99, -10, 100123, 18, -978,
                      5623, 463, -9, 287, 49};
-        int numsOK[] = {-978, -10, -9, 18, 49,
+        
+        Integer[] numsOK = {-978, -10, -9, 18, 49,
                      99, 287, 463, 5623, 100123};
         
         System.out.println("Current algorithm - <" + algorithm.GetName() + ">");
         
-        Display.IntDisplay("Current array:", nums);
+        Display.ConDisplay("Current array:", nums);
         
         System.out.println("Before sort. Test is " + IntCompare(nums, numsOK));
         
         algorithm.Sort(nums);
       
-        Display.IntDisplay("Sorted array:", nums);
+        Display.ConDisplay("Sorted array:", nums);
         
         System.out.println("After sort. Test is " + IntCompare(nums, numsOK));
     } 
