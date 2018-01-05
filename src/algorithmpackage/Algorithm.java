953c40ce4c9cd56d02c3cebc4b950a71376e4050
lost273 <dnk273@gmail.com>
2018-01-05 16:19:49 +0300

    05-01-2018

--- src/algorithmpackage/Algorithm.java
+++ src/algorithmpackage/Algorithm.java
@@ -4,6 +4,7 @@ package algorithmpackage;
 public abstract class Algorithm <T> {
     private String algorithmName;
     private T[] algorithmArray;
+    public abstract void Sort();
     public Algorithm(String name, T[] array){
         algorithmName = name;
         algorithmArray = array;
@@ -14,5 +15,11 @@ public abstract class Algorithm <T> {
     public String GetName(){
         return algorithmName;
     }
-    public abstract void Sort();
+    public boolean MoreOperator(int indexOne, int indexTwo){
+        if(algorithmArray.getClass().getName() == "java.lang.Integer"){
+        
+        }
+        int numberOne = algorithmArray[indexOne];
+        int numberTwo = algorithmArray[indexTwo];
+    }
 }
