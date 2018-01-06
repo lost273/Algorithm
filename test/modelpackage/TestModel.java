b9d36941e32add495b6b400d23623bcbe110e45e
lost273 <dnk273@gmail.com>
2018-01-06 17:15:19 +0300

    06-01-2018

--- test/modelpackage/TestModel.java
+++ test/modelpackage/TestModel.java
@@ -3,6 +3,7 @@ package modelpackage;
 
 import algorithmpackage.*;
 import displaypackage.*;
+import java.util.*;
 
 enum TypeOfArray {
     I,
@@ -15,6 +16,10 @@ public class TestModel {
         {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49},
         {'b','a','t','g','e','m','r','q','c'}
     };
+    private static Object[][] testArray = {
+        {-978, -10, -9, 18, 49, 99, 287, 463, 5623, 100123},
+        {'b','a','t','g','e','m','r','q','c'}
+    };
     
     private static <T> String IntCompare(T[] unsorted, T[] sorted){
         for(int i = 0; i < unsorted.length; i++){
@@ -24,23 +29,25 @@ public class TestModel {
         }
         return "SUCCESS";
     }
-    public static <T> void StartTest(Algorithm algorithm, String param){
-        Integer[] nums = {99, -10, 100123, 18, -978,
-                     5623, 463, -9, 287, 49};
+    public static <T> void StartTest(Algorithm algorithm){
+        System.out.println("Enter - I for Integet/ C for Char / D for Double:");
+        Scanner in = new Scanner(System.in);
+        String param = in.next();
         
-        Integer[] numsOK = {-978, -10, -9, 18, 49,
-                     99, 287, 463, 5623, 100123};
+        int arrayNumber = TypeOfArray.valueOf(param).ordinal();
+        Object[] a = commonArray[arrayNumber];
+        Object[] aTest = testArray[arrayNumber];
         
         System.out.println("Current algorithm - <" + algorithm.GetName() + ">");
         
         Display.ConDisplay("Current array:", a);
         
-        System.out.println("Before sort. Test is " + IntCompare(a, aOK));
+        System.out.println("Before sort. Test is " + IntCompare(a, aTest));
         
         algorithm.Sort(a);
       
         Display.ConDisplay("Sorted array:", a);
         
-        System.out.println("After sort. Test is " + IntCompare(a, aOK));
+        System.out.println("After sort. Test is " + IntCompare(a, aTest));
     } 
 }
