cb0c7a5d1a0f8bb44ac5d8aadbf27938891bbdc1
lost273 <dnk273@gmail.com>
2018-01-09 21:59:14 +0300

    09-01-2018

--- src/algorithmpackage/SortBubbleV2.java
+++ src/algorithmpackage/SortBubbleV2.java
@@ -1,12 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package algorithmpackage;
 
 
 public class SortBubbleV2 {
-
+    public static void main(String[] args){
+        int[] array = new int[] {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49}; 
+        for(int pass = 0; pass < array.length; pass++){
+            for(int bubble = array.length - 1; bubble > pass; bubble--){
+                if(array[bubble] < array[bubble-1]){
+                    int temp = array[bubble-1];
+                    array[bubble-1] = array[bubble];
+                    array[bubble] = temp;
+                }
+            }
+        }
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
 }
