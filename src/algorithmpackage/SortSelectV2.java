036d3d405147f6e05a5ceec77de057491b15a97e
lost273 <dnk273@gmail.com>
2018-01-09 22:24:02 +0300

    09-01-2018

---
+++ src/algorithmpackage/SortSelectV2.java
@@ -0,0 +1,12 @@
+
+package algorithmpackage;
+
+public class SortSelectV2 {
+    public static void main(String[] args){
+        int[] array = new int[] {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49}; 
+        
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+}
