4d8dc6d08ec88d5de17c2cddaa7af02cf1af9550
lost273 <dnk273@gmail.com>
2018-01-11 20:45:57 +0300

    11-01-2018

--- src/algorithmpackage/SortSelectV2.java
+++ src/algorithmpackage/SortSelectV2.java
@@ -4,9 +4,18 @@ package algorithmpackage;
 public class SortSelectV2 {
     public static void main(String[] args){
         int[] array = new int[] {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49}; 
-        int i, j, smaller, buffer;
-        for(i = 0; i < array.length; i++){
-        
+        int passSelect, smallerIndex, buffer;
+        for(int pass = 0; pass < array.length; pass++){
+            smallerIndex = pass;
+            buffer = array[pass];
+            for(passSelect = pass+1; passSelect < array.length; passSelect++){
+                if(array[passSelect] < buffer){
+                    smallerIndex = passSelect;
+                    buffer = array[passSelect];
+                }
+            }
+            array[smallerIndex] = array[pass];
+            array[pass] = buffer;
         }
         
         for(int a : array){
