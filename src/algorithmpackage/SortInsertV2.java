1e0b0ed14bab088a8122b705b9cb9c6e7379d301
lost273 <dnk273@gmail.com>
2018-01-12 21:56:42 +0300

    12-01-2018

--- src/algorithmpackage/SortInsertV2.java
+++ src/algorithmpackage/SortInsertV2.java
@@ -6,7 +6,11 @@ public class SortInsertV2 {
     public static void main(String[] args){
         int[] array = new int[] {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49}; 
         int ;
-        
+        for(){
+            for(){
+            
+            }
+        }
         
         for(int a : array){
             System.out.print(a + " ");
