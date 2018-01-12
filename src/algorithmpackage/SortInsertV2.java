e86345473e3ae6b2a600da9eb8ebc14cd1c2bd1e
lost273 <dnk273@gmail.com>
2018-01-12 22:03:46 +0300

    12-01-2018

--- src/algorithmpackage/SortInsertV2.java
+++ src/algorithmpackage/SortInsertV2.java
@@ -5,9 +5,10 @@ package algorithmpackage;
 public class SortInsertV2 {
     public static void main(String[] args){
         int[] array = new int[] {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49}; 
-        int ;
-        for(){
-            for(){
+        int leftIndex, buffer;
+        for(int passIndex = 0; passIndex < array.length; passIndex++){
+            buffer = array[passIndex];
+            for(leftIndex = pass - 1; (leftIndex >= 0) && ;){
             
             }
         }
